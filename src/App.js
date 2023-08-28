92c69d6021c5520ee3c940f76ed23f7947c543a6
Sachin Pal <sachinpal9352@gmial.com>
2023-08-28 20:50:08 +0530

    sww

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
- import React from "react";
+ import React, {useState} from "react";
  import TodoList from "./components/todolist";
  import AddItem from "./components/additem";
 import "./App.css";
  const App=()=>{
-    const listitems=[
-        {itemid :Math.trunc(Math.random()*100+1),itemname:"1. Buy Food"},
-        {itemid :Math.trunc(Math.random()*100+1),itemname:"2. Play"},
-        {itemid :Math.trunc(Math.random()*100+1),itemname:"3. Study"},
+    const [listitems, setListItems] = useState([
+        // {itemid :Math.trunc(Math.random()*100+1),itemname:"1. Buy Food"},
+        // {itemid :Math.trunc(Math.random()*100+1),itemname:"2. Play"},
+        // {itemid :Math.trunc(Math.random()*100+1),itemname:"3. Study"},
 
 
-    ];
+    ])
     const addItemHandler=(newitem)=>{
-      listitems.push(newitem);
-      console.log(listitems);
+
+      setListItems((previousList)=>{
+        return previousList.concat(newitem);
+
+      });
+      // listitems.push(newitem);
+      // console.log(listitems);
     }
       const name ="MERN";
     return (
